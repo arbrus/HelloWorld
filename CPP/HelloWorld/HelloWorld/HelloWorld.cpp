491a54dd7880d81046ec88464e0acab8415a3dbd
arbrus <ganker246@gmail.com>
2017-03-28 15:13:23 +0300

    HelloWorldCPP
    Wai not

---
+++ CPP/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,22 @@
+﻿/*
+Г¯͞¯͞¯͞͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ˥
+|███████████████████████████████████|
+|██MADE BY WINGIU███████████████████|
+|██KTO SPIZDIT██████████████████████|
+|██ILI SKOPIPASTIT PIDOR████████████|
+|██27.03.17█████████████████████████|
+|███████████████████████████████████|
+ᥨ___________________________________˩
+*/
+//da vdryg komy-to nyzen moi helloworld
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world";
+	return 0;
+}
+
